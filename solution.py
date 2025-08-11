619d2986dc080acfcdc82f369d7d1cd55d06f31b
shivam <skj522003@gmail.com>
2025-08-11 11:38:10 +0530

    first version

---
+++ solution.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+def load_players(file_path):
+    pass
+
+def load_matches(file_path):
+    pass
+
+def merge_players_matches(players_df, matches_df):
+   pass
+
+def total_runs_per_team(merged_df):
+   pass
+
+def calculate_strike_rate(merged_df):
+    pass
+
+def runs_agg_per_player(merged_df):
+   pass
+
+def avg_age_by_role(players_df):
+   pass
+def total_matches_per_player(matches_df):
+   pass
+
+    # Step 1: Get counts (index: PlayerID, column: count)
+    
+    # Step 2: Rename columns explicitly and cleanly
+   
+
+    # Ensure columns are in correct order
+   
+    return 
+
+
+
+def top_wicket_takers(merged_df):
+    pass
+def avg_strike_rate_per_team(merged_df):
+     pass
+
+def catch_to_match_ratio(merged_df):
+     pass
